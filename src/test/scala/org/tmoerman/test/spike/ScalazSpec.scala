16dd9de04f6c8afaec2e7219e3ad904a4fdaedd0
Thomas Moerman <thomas.moerman@gmail.com>
2015-11-05 15:34:50 +0100

    updated spike

--- src/test/scala/org/tmoerman/test/spike/ScalazSpec.scala
+++ src/test/scala/org/tmoerman/test/spike/ScalazSpec.scala
@@ -30,47 +30,60 @@ class ScalazSpec extends FlatSpec {
   import org.tmoerman.vcf.comp.util.Victorinox._
 
   "intersecting maps" should "bla" in {
-    val A = Map("a->t" -> List(1, 2), "c->g" -> List(4), "g->t" -> List(666), "k->l" -> List(99))
-    val B = Map("a->t" -> List(3,8), "c->g" -> List(5), "t->a" -> List(777))
+    val A = Map("a:t" -> List(1, 2), "c:g" -> List(4), "g:t" -> List(666), "k:l" -> List(99))
 
-    val concordant =
-      (A intersectWith B){ case pair => pair }
-        .toList
+    val B = Map("a:t" -> List(3,8), "c:g" -> List(5), "t:a" -> List(777))
+
+    val C = Map("prul" -> List(88))
+
+    def concordant2(mA: Map[String, List[Int]], mB: Map[String, List[Int]]) =
+      (mA intersectWith mB){ case pair => pair }
+        .toList match {
+          case Nil => Map()
+          case list =>
+            list
+              .map{ case (k, (a, b)) => ((k, a), (k, b))}
+              .unzip match { case (concA, concB) => Map(("A", "CONCORDANT") -> concA.toMap,
+                                                        ("B", "CONCORDANT") -> concB.toMap)
+              }
+          }
+
+    def concordant(mA: Map[String, List[Int]], mB: Map[String, List[Int]]) =
+      (mA intersectWith mB){ case pair => pair }
+        .toIterable
         .map{ case (k, (a, b)) => ((k, a), (k, b))}
-        .unzip match { case (concA, concB) =>
-          Map(("A", "CONCORDANT") -> concA.toMap,
-              ("B", "CONCORDANT") -> concB.toMap)
-        }
-
-    val concordant2 =
-      (A intersectWithKey B){ case (k, a, b) => ((k, a), (k, b)) }
-        .map(dropKey)
         .unzip match {
-          case (concA, concB) => Map (("A", "CONCORDANT") -> concA.toMap,
-                                      ("B", "CONCORDANT") -> concB.toMap)
-        }
+          case (Nil,   Nil  ) => Map()
+          case (concA, concB) => Map(("A", "CONCORDANT") -> concA.toMap,
+                                     ("B", "CONCORDANT") -> concB.toMap)
+          }
 
-    val discordant =
-      ((A -- B.keys).mapKeys(bc => (("A", "Disc"), bc)) ++
-       (B -- A.keys).mapKeys(bc => (("B", "Disc"), bc)))//(_ ++ _)
-        .toList // mapping over a List != mapping over a Map
-        .map{case ((cat, bc), discA) => Map(cat -> Map(bc -> discA))}
-        .reduce(_ |+| _)
-        //.reduce(_ ++ _)
+    val r = concordant(A, C)
 
-        //.mapValues(_.unzip)
+    println(r)
+  }
+
+}
+
+//    val concordant2 =
+//      (A intersectWithKey B){ case (k, a, b) => ((k, a), (k, b)) }
+//        .map(dropKey)
+//        .unzip match {
+//          case (concA, concB) => Map (("A", "CONCORDANT") -> concA.toMap,
+//                                      ("B", "CONCORDANT") -> concB.toMap)
+//        }
+//
+//    val discordant =
+//      ((A -- B.keys).mapKeys(bc => (("A", "Disc"), bc)) ++
+//       (B -- A.keys).mapKeys(bc => (("B", "Disc"), bc)))//(_ ++ _)
+//        .toList // mapping over a List != mapping over a Map
+//        .map{case ((cat, bc), discA) => Map(cat -> Map(bc -> discA))}
+//        .reduce(_ |+| _)
+//        //.reduce(_ ++ _)
+
+//.mapValues(_.unzip)
 
 //        .unzip match { case (concA, concB) =>
 //          (concA.map(_.head).map(e => ("A", e)),
 //           concB.map(_.head).map(e => ("B", e)))}
 
-    println(concordant)
-  }
-
-  "unioning maps" should "bla" in {
-
-
-
-  }
-
-}
