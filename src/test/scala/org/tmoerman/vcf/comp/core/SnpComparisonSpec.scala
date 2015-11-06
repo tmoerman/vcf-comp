ebbbb723118eb6891b84e82cc5edde2173b14dbc
Thomas Moerman <thomas.moerman@gmail.com>
2015-11-06 15:32:45 +0100

    fixed test

--- src/test/scala/org/tmoerman/vcf/comp/core/SnpComparisonSpec.scala
+++ src/test/scala/org/tmoerman/vcf/comp/core/SnpComparisonSpec.scala
@@ -3,7 +3,7 @@ package org.tmoerman.vcf.comp.core
 import org.tmoerman.test.spark.BaseSparkContextSpec
 
 import org.tmoerman.vcf.comp.VcfComparisonContext._
-import org.tmoerman.vcf.comp.core.SnpComparison.CONCORDANT
+import org.tmoerman.vcf.comp.core.SnpComparison.{VcfComparisonParams, CONCORDANT}
 
 /**
  * @author Thomas Moerman
@@ -12,14 +12,16 @@ class SnpComparisonSpec extends BaseSparkContextSpec {
 
   val annotated = "src/test/resources/small.snpEff.vcf"
 
-  val rdd = sc.startSnpComparison(annotated, annotated).cache()
+  val params = new VcfComparisonParams(unifyConcordant = true)
+
+  val rdd = sc.startSnpComparison(annotated, annotated, params).cache()
 
   "comparing the same VCF file" should "result in the correct amount of variants" in {
-    rdd.count shouldBe 133
+    rdd.count shouldBe 127 // not 133 because only SNPs are counted
   }
 
   "comparing the same VCF file" should "result in concordant genotypes only" in {
-    rdd.flatMap(identity).map(_._1).distinct().collect shouldBe Array(CONCORDANT)
+    rdd.map(_._1._2).distinct().collect shouldBe Array(CONCORDANT)
   }
 
 }
