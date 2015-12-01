3e9852805a6d7d8d91a5d54f618f16fbff8d21f2
Thomas Moerman <thomas.moerman@gmail.com>
2015-12-01 21:14:08 +0100

    specs update

--- src/test/scala/org/tmoerman/vcf/comp/core/TumorNormalMatchingSpec.scala
+++ src/test/scala/org/tmoerman/vcf/comp/core/TumorNormalMatchingSpec.scala
@@ -1,14 +1,11 @@
 package org.tmoerman.vcf.comp.core
 
-import java.io.File
-
 import org.tmoerman.adam.fx.avro.AnnotatedGenotype
 import org.tmoerman.test.spark.BaseSparkContextSpec
-import org.tmoerman.vcf.comp.core.Model.SnpComparisonParams
+import org.tmoerman.vcf.comp.core.Model.ComparisonParams
 import org.tmoerman.vcf.comp.VcfComparisonContext._
 import org.tmoerman.vcf.comp.core.Model._
 import org.bdgenomics.adam.rdd.ADAMContext._
-import org.tmoerman.adam.fx.snpeff.SnpEffContext._
 
 /**
  * @author Thomas Moerman
@@ -24,8 +21,8 @@ class TumorNormalMatchingSpec extends BaseSparkContextSpec{
     val m2 = (g: AnnotatedGenotype) => genotypeAlleles(g)
     val m3 = (g: AnnotatedGenotype) => (g.getGenotype.getIsPhased, genotypeAlleles(g))
 
-    val p2 = new SnpComparisonParams(labels = ("TUMOR", "NORMAL"), matchFunction = m2)
-    val p3 = new SnpComparisonParams(labels = ("TUMOR", "NORMAL"), matchFunction = m3)
+    val p2 = new ComparisonParams(labels = ("TUMOR", "NORMAL"), matchFunction = m2)
+    val p3 = new ComparisonParams(labels = ("TUMOR", "NORMAL"), matchFunction = m3)
 
     val rdd2 = sc.startSnpComparison(tumor, normal, p2).cache()
     val rdd3 = sc.startSnpComparison(tumor, normal, p3).cache()
@@ -35,7 +32,7 @@ class TumorNormalMatchingSpec extends BaseSparkContextSpec{
     println(rdd3.categoryCount)
   }
 
-  "loading non-annotated VCFs" should "work" in {
+  "loading non-annotated VCFs" should "work" ignore {
     val tumor  = wd + "4146_T.vcf.gz"
     val normal = wd + "4146_N.vcf.gz"
 
