5a5f87f1bba56c7865e90f6c92acbb5263761499
Thomas Moerman <thomas.moerman@gmail.com>
2015-12-01 14:07:27 +0100

    meh

--- src/test/scala/org/tmoerman/vcf/comp/core/TumorNormalMatchingSpec.scala
+++ src/test/scala/org/tmoerman/vcf/comp/core/TumorNormalMatchingSpec.scala
@@ -7,6 +7,8 @@ import org.tmoerman.test.spark.BaseSparkContextSpec
 import org.tmoerman.vcf.comp.core.Model.SnpComparisonParams
 import org.tmoerman.vcf.comp.VcfComparisonContext._
 import org.tmoerman.vcf.comp.core.Model._
+import org.bdgenomics.adam.rdd.ADAMContext._
+import org.tmoerman.adam.fx.snpeff.SnpEffContext._
 
 /**
  * @author Thomas Moerman
@@ -15,22 +17,29 @@ class TumorNormalMatchingSpec extends BaseSparkContextSpec{
 
   val wd = "/Users/tmo/Work/exascience/data/VCF-comp/tumor.normal/"
 
-  val tumor  = wd + "4146_T.vcf.gz.annotated.gz"
-  val normal = wd + "4146_N.vcf.gz.annotated.gz"
+  "loading annotated VCFs" should "work" in {
+    val tumor  = wd + "4146_T.vcf.gz.annotated.gz"
+    val normal = wd + "4146_N.vcf.gz.annotated.gz"
 
-  val m2 = (g: AnnotatedGenotype) => genotypeAlleles(g)
-  val m3 = (g: AnnotatedGenotype) => (g.getGenotype.getIsPhased, genotypeAlleles(g))
+    val m2 = (g: AnnotatedGenotype) => genotypeAlleles(g)
+    val m3 = (g: AnnotatedGenotype) => (g.getGenotype.getIsPhased, genotypeAlleles(g))
 
-  val p2 = new SnpComparisonParams(labels = ("TUMOR", "NORMAL"), matchFunction = m2)
-  val p3 = new SnpComparisonParams(labels = ("TUMOR", "NORMAL"), matchFunction = m3)
+    val p2 = new SnpComparisonParams(labels = ("TUMOR", "NORMAL"), matchFunction = m2)
+    val p3 = new SnpComparisonParams(labels = ("TUMOR", "NORMAL"), matchFunction = m3)
 
-  val rdd2 = sc.startSnpComparison(tumor, normal, p2).cache()
-  val rdd3 = sc.startSnpComparison(tumor, normal, p3).cache()
+    val rdd2 = sc.startSnpComparison(tumor, normal, p2).cache()
+    val rdd3 = sc.startSnpComparison(tumor, normal, p3).cache()
 
-  "bla" should "bla" in {
     println(rdd2.categoryCount)
 
     println(rdd3.categoryCount)
+  }
+
+  "loading non-annotated VCFs" should "work" in {
+    val tumor  = wd + "4146_T.vcf.gz"
+    val normal = wd + "4146_N.vcf.gz"
+
+    sc.loadVcf(tumor, None).take(10)
   }
 
 }
