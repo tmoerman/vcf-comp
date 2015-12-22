dadb0cea47e027ba0a3d33b2fba3ccf0b3e4fb6b
Thomas Moerman <thomas.moerman@gmail.com>
2015-12-22 15:12:29 +0100

    SNP comparison RDD functions annotations + docs

--- README.md
+++ README.md
@@ -342,11 +342,11 @@ This section contains an overview of all available analyses. Additionally, the [
 #### QC analyses
 
 ```Scala
-variantTypeCount
+def variantTypeCount
 ```
 
 ```Scala
-snpCountByContig
+def snpCountByContig
 ```
 
 ```Scala
@@ -375,63 +375,153 @@ def countByTraversableProjection[P: ClassTag](projection: VariantContext => Trav
 
 #### SNP comparison analyses
 
+
 ```Scala
+/**
+  * @param occurrences vararg. Accepts one or more Strings from "unique", "concordant", "discordant".
+  *
+  * @return Returns the RDD, filtered on the specified occurrences.
+  */
 def viewOnly(occurrences: String*)
 ```
 
 ```Scala
+/**
+  * @return Returns SNP count by concordance category.
+  */
 def categoryCount
 ```
 
 ```Scala
+/**
+  * @return Returns SNP count by base change per concordance category.
+  *
+  *         Base change is a String: "ref->alt" where ref and alt are the variant alleles, e.g. "A->T", "G->C", etc...
+  */
 def baseChangeCount
 ```
 
 ```Scala
+/**
+  * @return Returns SNP count by base change pattern per concordance category.
+  *
+  *         Base change pattern is a String: "ref:alt", analogous to base change, but without taking into account
+  *         the order of ref to alt, e.g. "A:T", "C:G", etc...
+  */
 def baseChangePatternCount
 ```
 
 ```Scala
+/**
+  * @return Returns SNP count by base change type per concordance category.
+  *
+  *         Base change types are "Ti" (Transition) and "Tv" (Transversion).
+  */
 def baseChangeTypeCount
 ```
 
 ```Scala
+/**
+  * @return Returns SNP count by zygosity per concordance category.
+  *
+  *         Zygosity values are: "HOM_REF", "HET", "HOM_ALT", "NO_CALL".
+  */
 def zygosityCount
 ```
 
 ```Scala
+/**
+  * @return Returns SNP count by functional impact per concordance category.
+  *
+  *         Functional impact is a scale value provided by SnpEff: "HIGH", "MODERATE", "LOW" and "MODIFIER".
+  *
+  *         Cfr. SnpEff http://snpeff.sourceforge.net/VCFannotationformat_v1.0.pdf
+  */
 def functionalImpactCount
 ```
 
 ```Scala
+/**
+  * @return Returns SNP count by functional annotation per concordance category.
+  *
+  *         Functional annotation is a SnpEff annotation, including: "synonymous_variant", "missense_variant",
+  *         "stop_gained", "start_lost".
+  *
+  *         Assumes annotation with SnpEff http://snpeff.sourceforge.net/VCFannotationformat_v1.0.pdf
+  */
 def functionalAnnotationCount
 ```
 
 ```Scala
+/**
+  * @return Returns SNP count by transcript biotype per concordance category.
+  *
+  *         Transcript biotype is a SnpEff annotation: including: "protein_coding", "retained_intron"
+  *         "nonsense_mediated_decay".
+  *
+  *         Assumes annotation with SnpEff http://snpeff.sourceforge.net/VCFannotationformat_v1.0.pdf
+  */
 def transcriptBiotypeCount
 ```
 
 ```Scala
-def clinvarRatio()
+/**
+  * @param label (optional). Maps the Boolean to a descriptive label.
+  *
+  * @return Returns the ratio of SNPs with a Clinvar annotation vs. SNPs without.
+  *
+  *         Assumes VCF annotation with SnpSift http://snpeff.sourceforge.net/SnpSift.html
+  */
+def clinvarRatio(label: Boolean => String)
 ```
 
 ```Scala
-def commonSnpRatio()
+/**
+  * @param label (optional). Maps the Boolean to a descriptive label.
+  *
+  * @return Returns the ratio of SNPs with a DbSNP annotation vs. SNPs without.
+  *
+  *         Assumes VCF annotation with SnpSift http://snpeff.sourceforge.net/SnpSift.html
+  */
+def commonSnpRatio(label: Boolean => String)
 ```
 
 ```Scala
-def synonymousRatio()
+/**
+  * @param label (optional). Maps the boolean to a descriptive label.
+  *
+  * @return Returns the ratio of SNPs with "synonymous_variant" vs. "missense_variant" annotation. If a SNP has
+  *         neither annotation, it is not taken into account.
+  *
+  *         Assumes annotation with SnpEff http://snpeff.sourceforge.net/VCFannotationformat_v1.0.pdf
+  */
+def synonymousRatio(label: Boolean => String)
 ```
 
 ```Scala
+/**
+  * @param step (optional). Size of the step interval for binning the read depth values.
+  *
+  * @return Returns the distribution of SNPs by read depth.
+  */
 def readDepthDistribution(step: Int = DEFAULT_READ_DEPTH_STEP)
 ```  
 
 ```Scala
+/**
+  * @param step (optional). Size of the step interval for binning the quality values.
+  *
+  * @return Returns the distribution of SNPs by quality.
+  */
 def qualityDistribution(step: Double = DEFAULT_QUALITY_STEP)
 ```
 
 ```Scala
+/**
+  * @param step (optional). Size of the step interval for binning the allele frequency values.
+  *
+  * @return Returns the distribution of SNPs by allele frequency.
+  */
 def alleleFrequencyDistribution(step: Double = DEFAULT_ALLELE_FREQUENCY_STEP)
 ```
 
@@ -472,3 +562,5 @@ VCF-comp makes use of a Scala idiom called [*"Pimp my library"*](http://www.arti
 ### Dimple.js
 
 `TODO`
+
+http://snpeff.sourceforge.net/VCFannotationformat_v1.0.pdf
--- src/main/scala/org/tmoerman/vcf/comp/core/SnpComparisonRDDFunctions.scala
+++ src/main/scala/org/tmoerman/vcf/comp/core/SnpComparisonRDDFunctions.scala
@@ -18,11 +18,19 @@ object SnpComparisonLabels {
 class SnpComparisonRDDFunctions(private[this] val rdd: RDD[OccurrenceRow[AnnotatedGenotype]]) extends Serializable with ApiHelp {
   import SnpComparisonLabels._
 
-  def viewOnly(occurrences: Occurrence*): RDD[OccurrenceRow[AnnotatedGenotype]] =
+  /**
+    * @param occurrences vararg. Accepts one or more Strings from "unique", "concordant", "discordant".
+    *
+    * @return Returns the RDD, filtered on the specified occurrences.
+    */
+  def viewOnly(occurrences: String*): RDD[OccurrenceRow[AnnotatedGenotype]] =
     rdd.map(row => row.filterKeys(occurrence =>
       occurrences.contains(occurrence) ||
       occurrences.map(_.toLowerCase).contains(occurrence.toLowerCase)))
 
+  /**
+    * @return Returns SNP count by concordance category.
+    */
   def categoryCount: Iterable[CategoryCount] =
     rdd
       .flatMap(flattenToReps())
@@ -30,35 +38,116 @@ class SnpComparisonRDDFunctions(private[this] val rdd: RDD[OccurrenceRow[Annotat
       .countByValue
       .map{ case (cat, count) => CategoryCount(cat, count) }
 
-  def baseChangeCount           = countByProjection(baseChangeString)
+  /**
+    * @return Returns SNP count by base change per concordance category.
+    *
+    *         Base change is a String: "ref->alt" where ref and alt are the variant alleles, e.g. "A->T", "G->C", etc...
+    */
+  def baseChangeCount = countByProjection(baseChangeString)
 
-  def baseChangePatternCount    = countByProjection(baseChangePatternString)
+  /**
+    * @return Returns SNP count by base change pattern per concordance category.
+    *
+    *         Base change pattern is a String: "ref:alt", analogous to base change, but without taking into account
+    *         the order of ref to alt, e.g. "A:T", "C:G", etc...
+    */
+  def baseChangePatternCount = countByProjection(baseChangePatternString)
 
-  def baseChangeTypeCount       = countByProjection(baseChangeType)
+  /**
+    * @return Returns SNP count by base change type per concordance category.
+    *
+    *         Base change types are "Ti" (Transition) and "Tv" (Transversion).
+    */
+  def baseChangeTypeCount = countByProjection(baseChangeType)
 
-  def zygosityCount             = countByTraversableProjection(zygosity)
+  /**
+    * @return Returns SNP count by zygosity per concordance category.
+    *
+    *         Zygosity values are: "HOM_REF", "HET", "HOM_ALT", "NO_CALL".
+    */
+  def zygosityCount = countByTraversableProjection(zygosity)
 
-  def functionalImpactCount     = countByProjection(functionalImpact)
+  /**
+    * @return Returns SNP count by functional impact per concordance category.
+    *
+    *         Functional impact is a scale value provided by SnpEff: "HIGH", "MODERATE", "LOW" and "MODIFIER".
+    *
+    *         Cfr. SnpEff http://snpeff.sourceforge.net/VCFannotationformat_v1.0.pdf
+    */
+  def functionalImpactCount = countByProjection(functionalImpact)
 
+  /**
+    * @return Returns SNP count by functional annotation per concordance category.
+    *
+    *         Functional annotation is a SnpEff annotation, including: "synonymous_variant", "missense_variant",
+    *         "stop_gained", "start_lost".
+    *
+    *         Assumes annotation with SnpEff http://snpeff.sourceforge.net/VCFannotationformat_v1.0.pdf
+    */
   def functionalAnnotationCount = countByProjection(functionalAnnotation)
 
-  def transcriptBiotypeCount    = countByProjection(transcriptBiotype)
+  /**
+    * @return Returns SNP count by transcript biotype per concordance category.
+    *
+    *         Transcript biotype is a SnpEff annotation: including: "protein_coding", "retained_intron"
+    *         "nonsense_mediated_decay".
+    *
+    *         Assumes annotation with SnpEff http://snpeff.sourceforge.net/VCFannotationformat_v1.0.pdf
+    */
+  def transcriptBiotypeCount = countByProjection(transcriptBiotype)
 
-  def clinvarRatio                (label: Boolean => String = CLINVAR_LABELS)    = countByProjection(g => label(hasClinvarAnnotations(g)))
+  /**
+    * @param label (optional). Maps the Boolean to a descriptive label.
+    *
+    * @return Returns the ratio of SNPs with a Clinvar annotation vs. SNPs without.
+    *
+    *         Assumes VCF annotation with SnpSift http://snpeff.sourceforge.net/SnpSift.html
+    */
+  def clinvarRatio(label: Boolean => String = CLINVAR_LABELS) = countByProjection(g => label(hasClinvarAnnotations(g)))
 
-  def commonSnpRatio              (label: Boolean => String = COMMON_SNP_LABELS) = countByProjection(g => label(hasDbSnpAnnotations(g)))
+  /**
+    * @param label (optional). Maps the Boolean to a descriptive label.
+    *
+    * @return Returns the ratio of SNPs with a DbSNP annotation vs. SNPs without.
+    *
+    *         Assumes VCF annotation with SnpSift http://snpeff.sourceforge.net/SnpSift.html
+    */
+  def commonSnpRatio(label: Boolean => String = COMMON_SNP_LABELS) = countByProjection(g => label(hasDbSnpAnnotations(g)))
 
-  def synonymousRatio             (label: Boolean => String = SYNONYMOUS_LABELS) = countByTraversableProjection(g => isSynonymous(g).map(label))
+  /**
+    * @param label (optional). Maps the boolean to a descriptive label.
+    *
+    * @return Returns the ratio of SNPs with "synonymous_variant" vs. "missense_variant" annotation. If a SNP has
+    *         neither annotation, it is not taken into account.
+    *
+    *         Assumes annotation with SnpEff http://snpeff.sourceforge.net/VCFannotationformat_v1.0.pdf
+    */
+  def synonymousRatio(label: Boolean => String = SYNONYMOUS_LABELS) = countByTraversableProjection(g => isSynonymous(g).map(label))
 
-  def readDepthDistribution       (step: ReadDepth = DEFAULT_READ_DEPTH_STEP)    = countByProjection(g => quantize(step)(readDepth(g)))
+  /**
+    * @param step (optional). Size of the step interval for binning the read depth values.
+    *
+    * @return Returns the distribution of SNPs by read depth.
+    */
+  def readDepthDistribution(step: Int = DEFAULT_READ_DEPTH_STEP) = countByProjection(g => quantize(step)(readDepth(g)))
 
-  def qualityDistribution         (step: Quality = DEFAULT_QUALITY_STEP)         = countByProjection(g => quantize(step)(quality(g)))
+  /**
+    * @param step (optional). Size of the step interval for binning the quality values.
+    *
+    * @return Returns the distribution of SNPs by quality.
+    */
+  def qualityDistribution(step: Double = DEFAULT_QUALITY_STEP) = countByProjection(g => quantize(step)(quality(g)))
 
-  def alleleFrequencyDistribution (step: Double = DEFAULT_ALLELE_FREQUENCY_STEP) = countByProjection(g => quantize(step)(alleleFrequency(g)))
+  /**
+    * @param step (optional). Size of the step interval for binning the allele frequency values.
+    *
+    * @return Returns the distribution of SNPs by allele frequency.
+    */
+  def alleleFrequencyDistribution(step: Double = DEFAULT_ALLELE_FREQUENCY_STEP) = countByProjection(g => quantize(step)(alleleFrequency(g)))
 
-  def zygositySwitchCount       = countBySwitch(zygosity)
+  def zygositySwitchCount = countBySwitch(zygosity)
 
-  def allelesSwitchCount        = countBySwitch(a => Some(genotypeAllelesString(a)))
+  def allelesSwitchCount = countBySwitch(a => Some(genotypeAllelesString(a)))
 
   def countByProjection[P](projection: AnnotatedGenotype => P): Iterable[CategoryProjectionCount[P]] =
     rdd
@@ -68,6 +157,7 @@ class SnpComparisonRDDFunctions(private[this] val rdd: RDD[OccurrenceRow[Annotat
       .countByValue
       .map{ case ((cat, p), count) => CategoryProjectionCount(cat, p, count) }
 
+
   def countByTraversableProjection[P](projection: AnnotatedGenotype => Traversable[P]): Iterable[CategoryProjectionCount[P]] =
     rdd
       .flatMap(flattenToReps())
@@ -75,6 +165,7 @@ class SnpComparisonRDDFunctions(private[this] val rdd: RDD[OccurrenceRow[Annotat
       .flatMapValues(projection(_))
       .countByValue
       .map{ case ((cat, p), count) => CategoryProjectionCount(cat, p, count) }
+
 
   def countBySwitch[P](projection: AnnotatedGenotype => Option[P]): Iterable[CategoryProjectionCount[String]] =
     rdd
